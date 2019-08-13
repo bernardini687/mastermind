c6e9dd6c5c434937bfa5fc4a034163a90816be95
Oscar Bernardini <bernardini687@gmail.com>
2019-08-13 17:25:49 +0200

    Format tests

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -13,29 +13,30 @@ module Mastermind
       before { @code = Code.new pegs: %i[dark blue blue orange green] }
 
       it 'returns correct evaluations of a code breaking attempt #1' do
-
-        expect(@code.evaluate %i[blue blue sky sky sky]).to eq %i[black white]
+        expect(@code.evaluate %i[blue blue sky sky sky])
+          .to eq %i[black white]
       end
 
       it 'returns correct evaluations of a code breaking attempt #2' do
-
-        expect(@code.evaluate %i[blue blue orange sky sky]).to eq %i[black white white]
+        expect(@code.evaluate %i[blue blue orange sky sky])
+          .to eq %i[black white white]
       end
 
       it 'returns correct evaluations of a code breaking attempt #4' do
-
-        expect(@code.evaluate %i[blue blue blue orange sky]).to eq %i[black black black]
+        expect(@code.evaluate %i[blue blue blue orange sky])
+          .to eq %i[black black black]
       end
 
       it 'returns correct evaluations of a code breaking attempt #5' do
-
-        expect(@code.evaluate %i[blue blue orange orange orange]).to eq %i[black black white]
+        expect(@code.evaluate %i[blue blue orange orange orange])
+          .to eq %i[black black white]
       end
 
       it 'returns correct evaluations of a code breaking attempt #6' do
         code = Code.new pegs: %i[blue pink pink green dark]
 
-        expect(code.evaluate %i[pink orange pink green blue]).to eq %i[black black white white]
+        expect(code.evaluate %i[pink orange pink green blue])
+          .to eq %i[black black white white]
       end
     end
   end
