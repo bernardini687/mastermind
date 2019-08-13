d033f6b59d2f57aa5765e485b3a50335a99310a9
Oscar Bernardini <bernardini687@gmail.com>
2019-08-13 17:21:17 +0200

    Test for code breaking attempts evaluation

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -8,5 +8,35 @@ module Mastermind
 
       expect(actual).to be_truthy
     end
+
+    describe '#evaluate' do
+      before { @code = Code.new pegs: %i[dark blue blue orange green] }
+
+      it 'returns correct evaluations of a code breaking attempt #1' do
+
+        expect(@code.evaluate %i[blue blue sky sky sky]).to eq %i[black white]
+      end
+
+      it 'returns correct evaluations of a code breaking attempt #2' do
+
+        expect(@code.evaluate %i[blue blue orange sky sky]).to eq %i[black white white]
+      end
+
+      it 'returns correct evaluations of a code breaking attempt #4' do
+
+        expect(@code.evaluate %i[blue blue blue orange sky]).to eq %i[black black black]
+      end
+
+      it 'returns correct evaluations of a code breaking attempt #5' do
+
+        expect(@code.evaluate %i[blue blue orange orange orange]).to eq %i[black black white]
+      end
+
+      it 'returns correct evaluations of a code breaking attempt #6' do
+        code = Code.new pegs: %i[blue pink pink green dark]
+
+        expect(code.evaluate %i[pink orange pink green blue]).to eq %i[black black white white]
+      end
+    end
   end
 end
