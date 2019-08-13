bf3dc5ef59a4a0ee790abf1169f25cd92d51131c
Oscar Bernardini <bernardini687@gmail.com>
2019-08-13 16:51:17 +0200

    Smoke test with colors

---
+++ lib/colors.rb
@@ -0,0 +1,3 @@
+module Mastermind
+  COLORS = %i[orange pink green yellow dark sky blue]
+end
---
+++ spec/mastermind_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Mastermind do
+  it 'has an array of objects' do
+    expect(Mastermind::COLORS).not_to be_nil
+  end
+end
